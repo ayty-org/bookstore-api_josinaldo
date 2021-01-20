c829e3bb45f55f3b7a95f8ba091f0db8dd4f44f8
Josinaldo Pontes <josinaldo.pontes@phoebus.local>
2021-01-20 17:05:15 -0300

    feat(#7): create Repository to Category and Client

---
+++ src/main/java/br/com/bookstore/bookstore/CategoryOfBook/CategoryOfBookRepository.java
@@ -0,0 +1,6 @@
+package br.com.bookstore.bookstore.CategoryOfBook;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryOfBookRepository extends JpaRepository<CategoryOfBook, Long> {
+}
--- src/main/java/br/com/bookstore/bookstore/book/BookDTO.java
+++
@@ -1,4 +0,0 @@
-package br.com.bookstore.bookstore.book;
-
-public class BookDTO {
-}
