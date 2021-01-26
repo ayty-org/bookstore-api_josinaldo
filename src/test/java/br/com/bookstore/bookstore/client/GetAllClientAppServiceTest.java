9bb9ff3fb972cdd492cd745b4af377afe951de73
josinaldojr <josinaldo.pontes@dcx.ufpb.br>
2021-01-26 14:46:33 -0300

    feat(#31): create test for GetAllClientAppService

--- src/test/java/br/com/bookstore/bookstore/client/GetAllClientAppServiceTest.java
+++ src/test/java/br/com/bookstore/bookstore/client/GetAllClientAppServiceTest.java
@@ -1,12 +1,57 @@
 package br.com.bookstore.bookstore.client;
 
+import br.com.bookstore.bookstore.client.services.GetAllClientAppServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static br.com.bookstore.bookstore.client.builders.ClientBuilder.createClient;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@Tag("Service")
+@DisplayName("Validates the functionality of the service responsible for list all client")
 class GetAllClientAppServiceTest {
+
+    @Mock
+    private ClientRepository clientRepository;
+
+    private GetAllClientAppServiceImpl getAllClientAppService;
 
     @BeforeEach
     void setUp() {
+        this.getAllClientAppService = new GetAllClientAppServiceImpl(clientRepository);
+    }
+
+    @Test
+    @DisplayName("listAll returns list of client when successful")
+    void listAll_ReturnsListOfClients_WhenSuccessfull() {
+
+        Client client = createClient().build();
+        when(clientRepository.findAll()).thenReturn(
+                Stream.of(createClient().name("Teste").build()).collect(Collectors.toList())
+        );
+
+        List<Client> result = this.getAllClientAppService.findAll();
+
+        assertAll("Client",
+                () -> assertThat(result.size(), is(1)),
+                () -> assertThat(result.get(0).getName(), is("Teste")),
+                () -> assertThat(result.get(0).getEmail(), is(client.getEmail())),
+                () -> assertThat(result.get(0).getPhone(), is(client.getPhone())),
+                () -> assertThat(result.get(0).getSexo(), is(client.getSexo()))
+        );
     }
 }
