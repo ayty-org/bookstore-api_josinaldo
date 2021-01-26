b8878071dcf483d98723f7bfa3e36062b6e6c11c
josinaldojr <josinaldo.pontes@dcx.ufpb.br>
2021-01-26 08:45:38 -0300

    feat(#28): create path builder to test client services

---
+++ src/test/java/br/com/bookstore/bookstore/client/builders/ClientBuilder.java
@@ -0,0 +1,17 @@
+package br.com.bookstore.bookstore.client.builders;
+
+import br.com.bookstore.bookstore.client.Client;
+
+public class ClientBuilder {
+
+    public static Client.Builder createClient() {
+        return Client
+                .builder()
+                .id(1L)
+                .name("Aktsuki")
+                .age(22)
+                .email("teste@email")
+                .phone("teste-phone")
+                .sexo("teste-sexo");
+    }
+}
