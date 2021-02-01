ffdb82e5a7a1a4ae6629d36cac4fe691d79b982a
josinaldojr <josinaldo.pontes@dcx.ufpb.br>
2021-02-01 06:45:56 -0300

    feat(#50): add Bean referencing to repository

--- src/main/java/br/com/bookstore/bookstore/client/ClientRepository.java
+++ src/main/java/br/com/bookstore/bookstore/client/ClientRepository.java
@@ -1,6 +1,8 @@
 package br.com.bookstore.bookstore.client;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
