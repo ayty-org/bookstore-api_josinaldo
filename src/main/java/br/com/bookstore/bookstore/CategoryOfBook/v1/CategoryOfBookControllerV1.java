70980afc3971a85af74093d4d5c084b4146b0824
josinaldojr <josinaldo.pontes@dcx.ufpb.br>
2021-02-01 11:29:33 -0300

    feat(#66): create CategoryOfBookControllerV1 and add method GET to find by id

---
+++ src/main/java/br/com/bookstore/bookstore/CategoryOfBook/v1/CategoryOfBookControllerV1.java
@@ -0,0 +1,33 @@
+package br.com.bookstore.bookstore.CategoryOfBook.v1;
+
+import br.com.bookstore.bookstore.CategoryOfBook.CategoryOfBook;
+import br.com.bookstore.bookstore.CategoryOfBook.CategoryOfBookDTO;
+import br.com.bookstore.bookstore.CategoryOfBook.service.DeleteCategoryOfBookService;
+import br.com.bookstore.bookstore.CategoryOfBook.service.GetCategoryOfBookService;
+import br.com.bookstore.bookstore.CategoryOfBook.service.ListCategoryOfBookService;
+import br.com.bookstore.bookstore.CategoryOfBook.service.ListPageCategoryOfBooksService;
+import br.com.bookstore.bookstore.CategoryOfBook.service.SaveCategoryOfBookService;
+import br.com.bookstore.bookstore.CategoryOfBook.service.UpdateCategoryOfBookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/v1/api/book/category")
+public class CategoryOfBookControllerV1 {
+
+    private final GetCategoryOfBookService getCategoryOfBookService;
+    private final UpdateCategoryOfBookService updateCategoryOfBookService;
+    private final ListCategoryOfBookService listCategoryOfBookService;
+    private final ListPageCategoryOfBooksService listPageCategoryOfBooksService;
+    private final DeleteCategoryOfBookService deleteCategoryOfBookService;
+    private final SaveCategoryOfBookService saveCategoryOfBookService;
+
+    @GetMapping(value = "/{id}") //list categoryofbook by id
+    public CategoryOfBookDTO find(@PathVariable Long id){
+        return CategoryOfBookDTO.from(getCategoryOfBookService.findById(id));
+    }
+}
