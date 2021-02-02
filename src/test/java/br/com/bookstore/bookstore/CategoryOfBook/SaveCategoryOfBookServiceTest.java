53cc648d44a2064845c22ebc983715887213249a
josinaldojr <josinaldo.pontes@dcx.ufpb.br>
2021-02-02 14:03:04 -0300

    feat(#82): create test SaveCategoryOfBookServiceTest

---
+++ src/test/java/br/com/bookstore/bookstore/CategoryOfBook/SaveCategoryOfBookServiceTest.java
@@ -0,0 +1,52 @@
+package br.com.bookstore.bookstore.CategoryOfBook;
+
+import br.com.bookstore.bookstore.CategoryOfBook.services.SaveCategoryOfBookServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static br.com.bookstore.bookstore.CategoryOfBook.builders.CategoryOfBookBuilder.createCategoryOfBook;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+@Tag("Service")
+@DisplayName("Validates the functionality of the services responsible for save category of book")
+class SaveCategoryOfBookServiceTest {
+
+    @Mock
+    private CategoryOfBookRepository categoryOfBookRepositoryMock;
+
+    private SaveCategoryOfBookServiceImpl saveCategoryOfBookService;
+
+    @BeforeEach
+    void setUp() {
+        this.saveCategoryOfBookService = new SaveCategoryOfBookServiceImpl(categoryOfBookRepositoryMock);
+    }
+
+    @Test
+    @DisplayName("save created category of books when successful")
+    void saveReturnsClientWhenSuccessful() {
+
+        CategoryOfBook categoryOfBook = createCategoryOfBook().build();
+
+        saveCategoryOfBookService.insert(categoryOfBook);
+        ArgumentCaptor<CategoryOfBook> captorCategoryOfBook = ArgumentCaptor.forClass(CategoryOfBook.class);
+
+        verify(categoryOfBookRepositoryMock, times(1)).save(captorCategoryOfBook.capture());
+
+        CategoryOfBook result = captorCategoryOfBook.getValue();
+
+        assertAll("CategoryOfBook",
+                () -> assertThat(result.getName(), is(categoryOfBook.getName()))
+        );
+    }
+}
