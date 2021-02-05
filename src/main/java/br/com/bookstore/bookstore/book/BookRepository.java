72a9cd20daba7c291322c1eed6481e4c5fe12058
josinaldojr <josinaldo.pontes@dcx.ufpb.br>
2021-02-05 10:02:26 -0300

    feat(#104): add method in repository to find by category in book

--- src/main/java/br/com/bookstore/bookstore/book/BookRepository.java
+++ src/main/java/br/com/bookstore/bookstore/book/BookRepository.java
@@ -1,6 +1,10 @@
 package br.com.bookstore.bookstore.book;
 
+import br.com.bookstore.bookstore.categoryOfBook.CategoryOfBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByCategory(CategoryOfBook category);
 }
