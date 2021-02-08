d9b8c68e6ab941c2c05c257afbe1b32e17daf8f1
josinaldojr <josinaldo.pontes@dcx.ufpb.br>
2021-02-08 09:21:11 -0300

    feat(#112): create SaveBookService and BookAlreadyExistException

--- src/main/java/br/com/bookstore/bookstore/book/BookRepository.java
+++ src/main/java/br/com/bookstore/bookstore/book/BookRepository.java
@@ -3,6 +3,9 @@ package br.com.bookstore.bookstore.book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByIsbn(String isbn);
 }
