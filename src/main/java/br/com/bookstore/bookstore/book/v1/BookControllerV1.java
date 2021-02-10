d12ca10a1b8651482c2bda53a8351809cb92b9d7
josinaldojr <josinaldo.pontes@dcx.ufpb.br>
2021-02-10 10:34:37 -0300

    feat(#126): add method delete in BookControllerV1

--- src/main/java/br/com/bookstore/bookstore/book/v1/BookControllerV1.java
+++ src/main/java/br/com/bookstore/bookstore/book/v1/BookControllerV1.java
@@ -14,9 +14,11 @@ import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -64,7 +66,15 @@ public class BookControllerV1 {
         saveBookService.insert(Book.to(bookDTO));
     }
 
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @PutMapping(value = "/{id}")
     public void update(@Valid @RequestBody BookDTO bookDTO, @PathVariable Long id){
         updateBookService.update(bookDTO, id);
+    }
+
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable Long id){
+        deleteBookService.delete(id);
     }
 }
