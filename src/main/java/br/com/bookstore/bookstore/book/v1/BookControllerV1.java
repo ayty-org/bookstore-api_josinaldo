cc9f1955d7f2e564accac4a3eec1443c427bfcb4
josinaldojr <josinaldo.pontes@dcx.ufpb.br>
2021-02-10 10:24:57 -0300

    feat(#124): add method findPage and insert in BookControllerV1

--- src/main/java/br/com/bookstore/bookstore/book/v1/BookControllerV1.java
+++ src/main/java/br/com/bookstore/bookstore/book/v1/BookControllerV1.java
@@ -1,5 +1,6 @@
 package br.com.bookstore.bookstore.book.v1;
 
+import br.com.bookstore.bookstore.book.Book;
 import br.com.bookstore.bookstore.book.BookDTO;
 import br.com.bookstore.bookstore.book.services.DeleteBookService;
 import br.com.bookstore.bookstore.book.services.GetBookService;
@@ -9,11 +10,19 @@ import br.com.bookstore.bookstore.book.services.ListPageBookService;
 import br.com.bookstore.bookstore.book.services.SaveBookService;
 import br.com.bookstore.bookstore.book.services.UpdateBookService;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.api.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -42,5 +51,16 @@ public class BookControllerV1 {
     @GetMapping(path = "/{categoryname}")
     public List<BookDTO> findAllBooksByCategoryName(@PathVariable String categoryname){
         return BookDTO.fromAll(listBookByCategoryService.findAllBooksByCategoryName(categoryname));
+    }
+
+    @GetMapping(path = {"/"})
+    public Page<BookDTO> findPage(@ParameterObject Pageable pageable) {
+        return BookDTO.fromPage(listPageBookService.findPage(pageable));
+    }
+
+    @ResponseStatus(code = HttpStatus.CREATED)
+    @PostMapping
+    public void insert(@Valid @RequestBody BookDTO bookDTO){
+        saveBookService.insert(Book.to(bookDTO));
     }
 }
